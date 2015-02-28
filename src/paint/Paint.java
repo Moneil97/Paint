5f0b8a994326299fdca1fd1a8f9feaf9b3e73698
Moneil <cam8059@yahoo.com>
2015-02-27 22:59:00 -0600

    Frame Setup

--- src/paint/Paint.java
+++ src/paint/Paint.java
@@ -1,13 +1,46 @@
 package paint;
 
-public class Paint {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+
+public class Paint extends JFrame{
 
 	public Paint() {
+		
+		setupFrame();
+		addListeners();
+		
+	}
+
+	
+
+	private void setupFrame() {
+		JPanel centerPanel = new CenterPanel();
+		JPanel topPanel = new TopPanel();
+		JPanel bottomPanel = new BottomPanel();
+		JPanel leftPanel = new LeftPanel();
+		JPanel rightPanel = new RightPanel();
+		
+		getContentPane().add(centerPanel, BorderLayout.CENTER);
+		getContentPane().add(topPanel, BorderLayout.NORTH);
+		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+		getContentPane().add(leftPanel, BorderLayout.WEST);
+		getContentPane().add(rightPanel, BorderLayout.EAST);
+		
+		this.setSize(400, 400);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+	private void addListeners() {
 		
 	}
 
 	public static void main(String[] args) {
-		
+		new Paint();
 	}
 
 }
